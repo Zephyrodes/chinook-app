2ae80ac99c2f3ed80e02938c330046fb880a9522
Ubuntu <ubuntu@ip-172-31-81-82.ec2.internal>
2025-11-28 06:06:08 +0000

    update database.py

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,25 +1,22 @@
 import os
-from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-load_dotenv()  # <- esto carga el .env
-
 Base = declarative_base()
 
-DB_HOST = os.getenv("DB_HOST")
-DB_USER = os.getenv("DB_USER")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_NAME = os.getenv("DB_NAME")
+DB_HOST = os.environ["DB_HOST"]
+DB_USER = os.environ["DB_USER"]
+DB_PASSWORD = os.environ["DB_PASSWORD"]
+DB_NAME = os.environ["DB_NAME"]
 
 DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:3306/{DB_NAME}"
 
 engine = create_engine(
     DATABASE_URL,
-    pool_size=int(os.getenv("DB_POOL_SIZE", 10)),
-    max_overflow=int(os.getenv("DB_MAX_OVERFLOW", 20)),
-    pool_timeout=int(os.getenv("DB_POOL_TIMEOUT", 30)),
-    echo=os.getenv("SQL_ECHO", "false").lower() == "true"
+    pool_size=int(os.environ.get("DB_POOL_SIZE", 10)),
+    max_overflow=int(os.environ.get("DB_MAX_OVERFLOW", 20)),
+    pool_timeout=int(os.environ.get("DB_POOL_TIMEOUT", 30)),
+    echo=os.environ.get("SQL_ECHO", "false").lower() == "true"
 )
 
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
